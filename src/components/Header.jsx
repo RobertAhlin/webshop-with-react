392129a8014e56565c62440cd68bf730db7f5a4b
Robert Ahlin <rob@ahlin.org>
2024-06-05 13:40:22 +0200

    Worked on minor visual improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,22 @@
 // src/components/Header.jsx
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Header.css'; // Import the CSS file
+import './Header.css';
 
 const Header = () => {
     return (
-        <header className="center">
-            <div className="logo">All & All Webshop</div>
-            <nav>
-                <ul className="nav-links">
-                    <li><NavLink to="/" className="nav-link" activeclassname="active">Home</NavLink></li>
-                    <li><NavLink to="/about" className="nav-link" activeclassname="active">About</NavLink></li>
-                    <li><NavLink to="/checkout" className="nav-link" activeclassname="active">Checkout</NavLink></li>
-                </ul>
-            </nav>
-        </header>
+        <div className="header">
+            <header className="center">
+                <div className="logo">All & All Webshop</div>
+                <nav>
+                    <ul className="nav-links">
+                        <li><NavLink to="/" className="nav-link" activeclassname="active">Home</NavLink></li>
+                        <li><NavLink to="/about" className="nav-link" activeclassname="active">About</NavLink></li>
+                        <li><NavLink to="/checkout" className="nav-link" activeclassname="active">Checkout</NavLink></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
     );
 };
 
