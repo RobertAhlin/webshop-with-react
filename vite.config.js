8371ac3dac2e550bdec27d57c9a81560c597edf8
Robert Ahlin <rob@ahlin.org>
2024-06-10 12:18:11 +0200

    Modified index.html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
+//vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://robertahlin.github.io/webshop-with-react/',
+  base: '/webshop-with-react/',
 })
