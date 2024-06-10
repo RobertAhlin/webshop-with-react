18e08a5de40d0c0c5aa086ab3d7118ffec167732
Robert Ahlin <rob@ahlin.org>
2024-06-10 12:44:32 +0200

    Deployed

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './App.css'
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,5 +12,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
